c0eae88c3fbdd62242ef264766a6049d7fc94065
Bruno Biachi <bruno.markethero@gmail.com>
2019-08-23 13:19:48 -0300

    asd

--- simple_app.py
+++ simple_app.py
@@ -83,4 +83,4 @@ def listening(**param):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=33507, host='0.0.0.0')
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)) , host='0.0.0.0')
